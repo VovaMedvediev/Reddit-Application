bf3fa8911d137a8bec352fe04b8e19dea75b98df
Vladimir Medvediev <vovkamedvedev5@gmail.com>
2017-11-03 23:10:45 +0200

    Added model for signing in into Reddit.

--- app/src/main/java/com/example/redditapp/Account/LoginActivity.java
+++ app/src/main/java/com/example/redditapp/Account/LoginActivity.java
@@ -4,8 +4,26 @@ import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.Toast;
 
+import com.example.redditapp.Comments.CommentsActivity;
+import com.example.redditapp.FeedAPI;
 import com.example.redditapp.R;
+import com.example.redditapp.URLS;
+import com.example.redditapp.model.Feed;
+
+import java.util.HashMap;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+import retrofit2.converter.simplexml.SimpleXmlConverterFactory;
 
 /**
  * Created by user on 01.11.2017.
@@ -14,11 +32,59 @@ import com.example.redditapp.R;
 public class LoginActivity extends AppCompatActivity {
 
     private static final String TAG = "LoginActivity";
+    private ProgressBar mProgressBar;
+    private EditText mUsername;
+    private EditText mPassword;
+    private URLS urls = new URLS();
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
         Log.d(TAG, "onCreate: started.");
+        Button btnLogin = (Button) findViewById(R.id.btn_login);
+        mPassword = (EditText) findViewById(R.id.input_password);
+        mUsername = (EditText) findViewById(R.id.input_username);
+        mProgressBar = (ProgressBar) findViewById(R.id.loginRequestLoadingProgressBar);
+        mProgressBar.setVisibility(View.GONE);
+
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.d(TAG, "onClick: Attempting to log in.");
+                String username = mUsername.getText().toString();
+                String password = mPassword.getText().toString();
+
+                if(!username.equals("") && !password.equals("")) {
+                    mProgressBar.setVisibility(View.VISIBLE);
+                }
+            }
+        });
+    }
+
+    private void login(String username, String password){
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl(urls.LOGIN_URL)
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
+
+        FeedAPI feedAPI = retrofit.create(FeedAPI.class);
+
+        HashMap<String, String> headerMap = new HashMap<>();
+        headerMap.put("Content-type", "application/json");
+
+        Call<CheckLogin> call = feedAPI.signIn(headerMap, username, username, password, "json");
+        call.enqueue(new Callback<CheckLogin>() {
+            @Override
+            public void onResponse(Call<CheckLogin> call, Response<CheckLogin> response) {
+                Log.d(TAG, "onResponse: Server response: " + response.toString());
+            }
+
+            @Override
+            public void onFailure(Call<CheckLogin> call, Throwable t) {
+                Log.e(TAG, "onFailure: Unable to login" + t.getMessage() );
+                Toast.makeText(LoginActivity.this, "An Error Occured", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
